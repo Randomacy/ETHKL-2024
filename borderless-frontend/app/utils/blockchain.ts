7cc769f5f0841c612c7a1adcb195063831e06917
Jackie <jackietanyen@gmail.com>
2024-10-05 01:44:32 +0800

    Update blockchain.ts

--- borderless-frontend/app/utils/blockchain.ts
+++ borderless-frontend/app/utils/blockchain.ts
@@ -1,129 +1,552 @@
-import Web3 from 'web3';
-import { AbiItem } from 'web3-utils';
+import Web3 from "web3";
+import { AbiItem } from "web3-utils";
 
 // Replace with your actual contract ABI and address
 const contractABI: AbiItem[] = [
   {
-    "inputs": [
+    type: "constructor",
+    inputs: [
       {
-        "internalType": "address",
-        "name": "recipient",
-        "type": "address"
+        name: "initialOwner",
+        type: "address",
+        internalType: "address",
       },
       {
-        "internalType": "uint256",
-        "name": "amount",
-        "type": "uint256"
-      }
+        name: "trustedForwarder",
+        type: "address",
+        internalType: "address",
+      },
     ],
-    "name": "transfer",
-    "outputs": [
-      {
-        "internalType": "bool",
-        "name": "",
-        "type": "bool"
-      }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    stateMutability: "nonpayable",
   },
   {
-    "inputs": [
+    type: "function",
+    name: "allowance",
+    inputs: [
       {
-        "internalType": "address",
-        "name": "account",
-        "type": "address"
+        name: "owner",
+        type: "address",
+        internalType: "address",
       },
       {
-        "internalType": "uint256",
-        "name": "amount",
-        "type": "uint256"
-      }
+        name: "spender",
+        type: "address",
+        internalType: "address",
+      },
     ],
-    "name": "mint",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    outputs: [
+      {
+        name: "",
+        type: "uint256",
+        internalType: "uint256",
+      },
+    ],
+    stateMutability: "view",
   },
   {
-    "inputs": [
+    type: "function",
+    name: "approve",
+    inputs: [
       {
-        "internalType": "address",
-        "name": "account",
-        "type": "address"
+        name: "spender",
+        type: "address",
+        internalType: "address",
       },
       {
-        "internalType": "uint256",
-        "name": "amount",
-        "type": "uint256"
-      }
+        name: "value",
+        type: "uint256",
+        internalType: "uint256",
+      },
     ],
-    "name": "burn",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  }
+    outputs: [
+      {
+        name: "",
+        type: "bool",
+        internalType: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "balanceOf",
+    inputs: [
+      {
+        name: "account",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+    outputs: [
+      {
+        name: "",
+        type: "uint256",
+        internalType: "uint256",
+      },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "burn",
+    inputs: [
+      {
+        name: "amount",
+        type: "uint256",
+        internalType: "uint256",
+      },
+    ],
+    outputs: [],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "decimals",
+    inputs: [],
+    outputs: [
+      {
+        name: "",
+        type: "uint8",
+        internalType: "uint8",
+      },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "isTrustedForwarder",
+    inputs: [
+      {
+        name: "forwarder",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+    outputs: [
+      {
+        name: "",
+        type: "bool",
+        internalType: "bool",
+      },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "mint",
+    inputs: [
+      {
+        name: "to",
+        type: "address",
+        internalType: "address",
+      },
+      {
+        name: "amount",
+        type: "uint256",
+        internalType: "uint256",
+      },
+    ],
+    outputs: [],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "name",
+    inputs: [],
+    outputs: [
+      {
+        name: "",
+        type: "string",
+        internalType: "string",
+      },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "owner",
+    inputs: [],
+    outputs: [
+      {
+        name: "",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "renounceOwnership",
+    inputs: [],
+    outputs: [],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "symbol",
+    inputs: [],
+    outputs: [
+      {
+        name: "",
+        type: "string",
+        internalType: "string",
+      },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "totalSupply",
+    inputs: [],
+    outputs: [
+      {
+        name: "",
+        type: "uint256",
+        internalType: "uint256",
+      },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "transfer",
+    inputs: [
+      {
+        name: "to",
+        type: "address",
+        internalType: "address",
+      },
+      {
+        name: "value",
+        type: "uint256",
+        internalType: "uint256",
+      },
+    ],
+    outputs: [
+      {
+        name: "",
+        type: "bool",
+        internalType: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "transferFrom",
+    inputs: [
+      {
+        name: "from",
+        type: "address",
+        internalType: "address",
+      },
+      {
+        name: "to",
+        type: "address",
+        internalType: "address",
+      },
+      {
+        name: "value",
+        type: "uint256",
+        internalType: "uint256",
+      },
+    ],
+    outputs: [
+      {
+        name: "",
+        type: "bool",
+        internalType: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "transferOwnership",
+    inputs: [
+      {
+        name: "newOwner",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+    outputs: [],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "trustedForwarder",
+    inputs: [],
+    outputs: [
+      {
+        name: "",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "event",
+    name: "Approval",
+    inputs: [
+      {
+        name: "owner",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+      {
+        name: "spender",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+      {
+        name: "value",
+        type: "uint256",
+        indexed: false,
+        internalType: "uint256",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "OwnershipTransferred",
+    inputs: [
+      {
+        name: "previousOwner",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+      {
+        name: "newOwner",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "Transfer",
+    inputs: [
+      {
+        name: "from",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+      {
+        name: "to",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+      {
+        name: "value",
+        type: "uint256",
+        indexed: false,
+        internalType: "uint256",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "error",
+    name: "ERC20InsufficientAllowance",
+    inputs: [
+      {
+        name: "spender",
+        type: "address",
+        internalType: "address",
+      },
+      {
+        name: "allowance",
+        type: "uint256",
+        internalType: "uint256",
+      },
+      {
+        name: "needed",
+        type: "uint256",
+        internalType: "uint256",
+      },
+    ],
+  },
+  {
+    type: "error",
+    name: "ERC20InsufficientBalance",
+    inputs: [
+      {
+        name: "sender",
+        type: "address",
+        internalType: "address",
+      },
+      {
+        name: "balance",
+        type: "uint256",
+        internalType: "uint256",
+      },
+      {
+        name: "needed",
+        type: "uint256",
+        internalType: "uint256",
+      },
+    ],
+  },
+  {
+    type: "error",
+    name: "ERC20InvalidApprover",
+    inputs: [
+      {
+        name: "approver",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+  },
+  {
+    type: "error",
+    name: "ERC20InvalidReceiver",
+    inputs: [
+      {
+        name: "receiver",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+  },
+  {
+    type: "error",
+    name: "ERC20InvalidSender",
+    inputs: [
+      {
+        name: "sender",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+  },
+  {
+    type: "error",
+    name: "ERC20InvalidSpender",
+    inputs: [
+      {
+        name: "spender",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+  },
+  {
+    type: "error",
+    name: "OwnableInvalidOwner",
+    inputs: [
+      {
+        name: "owner",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+  },
+  {
+    type: "error",
+    name: "OwnableUnauthorizedAccount",
+    inputs: [
+      {
+        name: "account",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+  },
 ];
-const contractAddress = '0x0C5d6E1B71f68eBf92c6Ab1e7017c59966F5853F';
+const contractAddress = "0xBfB046099a45A6c556aa3B025a4692a17a123632";
 
 // Initialize Web3 with your Ethereum node URL
-const web3 = new Web3('https://pacific-rpc.manta.network/http');
+const web3 = new Web3("https://pacific-rpc.sepolia-testnet.manta.network/http");
 
 const contract = new web3.eth.Contract(contractABI, contractAddress);
 
-export async function transferTokens(fromAddress: string, toAddress: string, amount: string) {
+export async function transferTokens(
+  fromAddress: string,
+  toAddress: string,
+  amount: string
+) {
   try {
-    const amountInWei = web3.utils.toWei(amount, 'ether');
+    const amountInWei = web3.utils.toWei(amount, "ether");
     const gasPrice = await web3.eth.getGasPrice();
-    const gas = await contract.methods.transfer(toAddress, amountInWei).estimateGas({ from: fromAddress });
+    const gas = await contract.methods
+      .transfer(toAddress, amountInWei)
+      .estimateGas({ from: fromAddress });
 
     const tx = await contract.methods.transfer(toAddress, amountInWei).send({
       from: fromAddress,
       gas,
-      gasPrice
+      gasPrice,
     });
 
     return tx.transactionHash;
   } catch (error) {
-    console.error('Error transferring tokens:', error);
+    console.error("Error transferring tokens:", error);
     throw error;
   }
 }
 
 export async function mintTokens(toAddress: string, amount: string) {
   try {
-    const amountInWei = web3.utils.toWei(amount, 'ether');
+    const amountInWei = web3.utils.toWei(amount, "ether");
     const gasPrice = await web3.eth.getGasPrice();
-    const gas = await contract.methods.mint(toAddress, amountInWei).estimateGas();
+    const gas = await contract.methods
+      .mint(toAddress, amountInWei)
+      .estimateGas();
 
     const tx = await contract.methods.mint(toAddress, amountInWei).send({
-      from: await web3.eth.getAccounts().then(accounts => accounts[0]), // Assuming the first account is the contract owner
+      from: await web3.eth.getAccounts().then((accounts) => accounts[0]), // Assuming the first account is the contract owner
       gas,
-      gasPrice
+      gasPrice,
     });
 
     return tx.transactionHash;
   } catch (error) {
-    console.error('Error minting tokens:', error);
+    console.error("Error minting tokens:", error);
     throw error;
   }
 }
 
 export async function burnTokens(fromAddress: string, amount: string) {
   try {
-    const amountInWei = web3.utils.toWei(amount, 'ether');
+    const amountInWei = web3.utils.toWei(amount, "ether");
     const gasPrice = await web3.eth.getGasPrice();
-    const gas = await contract.methods.burn(fromAddress, amountInWei).estimateGas({ from: fromAddress });
+    const gas = await contract.methods
+      .burn(fromAddress, amountInWei)
+      .estimateGas({ from: fromAddress });
 
     const tx = await contract.methods.burn(fromAddress, amountInWei).send({
       from: fromAddress,
       gas,
-      gasPrice
+      gasPrice,
     });
 
     return tx.transactionHash;
   } catch (error) {
-    console.error('Error burning tokens:', error);
+    console.error("Error burning tokens:", error);
+    throw error;
+  }
+}
+
+export async function getTokenBalance(address: string): Promise<string> {
+  try {
+    const balance = await contract.methods.balanceOf(address).call();
+    const balanceInEther = web3.utils.fromWei(balance, "ether");
+    // Convert to number and format to two decimal places
+    return parseFloat(balanceInEther).toFixed(2);
+  } catch (error) {
+    console.error("Error getting token balance:", error);
     throw error;
   }
 }
